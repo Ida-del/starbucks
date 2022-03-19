eb691c5fe061ccde4baf01d29276dafc77dce300
Ida-del <sycchem@gmail.com>
2022-03-19 18:22:11 +0900

    Separation Common Module

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
-const searchEl = document.querySelector('.search')
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click',function() {
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder','통합검색')
-});
-
-searchInputEl.addEventListener('blur', function () {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder','')
-});
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
-window.addEventListener('scroll', _.throttle(function(){
+window.addEventListener('scroll', _.throttle(function () {
     if(window.scrollY > 500) {
         // gsap.to(요소, 지속시간, 옵션)
         gsap.to(badgeEl, .6, {
@@ -142,7 +125,3 @@ spyEls.forEach(function (spyEl){
     .setClassToggle(spyEl,'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
